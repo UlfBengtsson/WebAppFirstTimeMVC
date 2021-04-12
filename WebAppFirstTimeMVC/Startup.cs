ecd2616b8c19c99999395afaf32df1f7a505edde
Ulf Bengtsson <Ulf.Bengtsson.Lexicon@outlook.com>
2021-04-12 11:59:46 +0200

    move over to MVC

--- WebAppFirstTimeMVC/Startup.cs
+++ WebAppFirstTimeMVC/Startup.cs
@@ -16,6 +16,8 @@ namespace WebAppFirstTimeMVC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            //services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +28,22 @@ namespace WebAppFirstTimeMVC
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.UseDefaultFiles();//for hosting static html files
+            app.UseStaticFiles();//for files inside the wwwroot folder
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
             });
         }
     }
