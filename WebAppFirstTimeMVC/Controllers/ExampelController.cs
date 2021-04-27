858fb3dcbb11670d15ff10fc6b1038757db7b0ff
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2021-04-27 11:16:00 +0200

    added exampel for Form Checkbox and Radio inputs + some bootstrap css

---
+++ WebAppFirstTimeMVC/Controllers/ExampelController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppFirstTimeMVC.Models.ViewModel;
+
+namespace WebAppFirstTimeMVC.Controllers
+{
+    public class ExampelController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new ExampelViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(ExampelViewModel exampelViewModel)
+        {
+            return View(exampelViewModel);
+        }
+    }
+}
---
+++ WebAppFirstTimeMVC/Models/ViewModel/ExampelViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppFirstTimeMVC.Models.Data;
+
+namespace WebAppFirstTimeMVC.Models.ViewModel
+{
+    public class ExampelViewModel
+    {
+        public string SomeText { get; set; }
+        public bool IsActive { get; set; }
+        public Gender GenderStatus { get; set; }
+    }
+}
